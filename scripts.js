769b00655a2ec6655ed6aced34aefa4af5c9ab28
Kyle Patton <pattonkb0011@gmail.com>
2019-05-01 12:09:09 +0100

    Commenting out js for now

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
-const button = document.querySelector(".button");
-const pokemon = document.querySelector(".pokemon");
-
+// const button = document.querySelector(".poke-viewer__item--poke-speak");
+// const pokemon = document.querySelector(".pokemon");
+//
+// function byPass() {
+//     var cors_api_host = 'cors-anywhere.herokuapp.com';
+//     var cors_api_url = 'https://' + cors_api_host + '/';
+//     var slice = [].slice;
+//     var origin = window.location.protocol + '//' + window.location.host;
+//     var open = XMLHttpRequest.prototype.open;
+//     XMLHttpRequest.prototype.open = function() {
+//         var args = slice.call(arguments);
+//         var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+//         if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+//             targetOrigin[1] !== cors_api_host) {
+//             args[1] = cors_api_url + args[1];
+//         }
+//         return open.apply(this, args);
+//     };
+// }
+// byPass();
+//
 // button.addEventListener("click", (e) => {
 //     e.preventDefault();
 //     const pokeRequest = new XMLHttpRequest();
@@ -9,30 +27,25 @@ const pokemon = document.querySelector(".pokemon");
 //             console.log("test");
 //             const pokeResponse = JSON.parse(pokeRequest.responseText);
 //             console.log(pokeResponse);
-//             // const pokeHTML = `
-//             //     <h1>${pokeResponse.species.name}</h1>
-//             //     <img src="${pokeResponse.sprites.front_default}" />
-//             // `;
-//             // pokemon.innerHTML = pokeHTML;
 //         }
 //     };
 //     pokeRequest.open("GET","http://pokeapi.co/api/v2/pokemon/?limit=151");
 //     pokeRequest.send();
 // });
 // const axios = require('axios');
-
-axios.get('http://pokeapi.co/api/v2/pokemon/?limit=151')
-  .then (response => {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-
-  axios.get('http://pokeapi.co/api/v2/pokemon/squirtle')
-    .then (response => {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
+//
+// axios.get('http://pokeapi.co/api/v2/pokemon/?limit=151')
+//   .then (response => {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//
+//   axios.get('http://pokeapi.co/api/v2/pokemon/squirtle')
+//     .then (response => {
+//       console.log(response);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     })
