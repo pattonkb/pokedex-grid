fd71db12ce8629d65f3197eac4207310c29a2d3e
Kyle Patton <pattonkb0011@gmail.com>
2019-05-17 21:46:15 -0400

    updated text-to-speech
    also adjusted some styling and html layouts

--- scripts.js
+++ scripts.js
@@ -17,8 +17,7 @@ function byPass() {
 byPass();
 
 function getRandomPokemon() {
-    let randomPokemon = Math.floor(Math.random() * (151 - 0 + 1)) + 1;
-    // let pokemon = 1;
+    let randomPokemon = Math.floor((Math.random()*150)+1);
     return randomPokemon;
 }
 
@@ -31,6 +30,7 @@ function capitalizeFirst(word) {
 }
 
 const pokeGenInfo = document.querySelector(".poke__general-info");
+const pokeViewer = document.querySelector(".poke-viewer__item--poke-image-container");
 const pokeImage = document.querySelector(".poke-viewer__item--poke-image");
 const getRandomizedPokemon = document.querySelector(".poke-controls__item--enter");
 const pokeDescShort = document.querySelector(".poke-controls__item--description-short");
@@ -44,37 +44,38 @@ getRandomizedPokemon.addEventListener("click", function() {
       if (pokeRequest.readyState === 4 && pokeRequest.status === 200) {
           const pokeResponse = JSON.parse(pokeRequest.responseText);
           console.log(pokeResponse);
-          pokeImage.src = pokeResponse.sprites.front_default;
+          pokeViewer.innerHTML = `<img src="${pokeResponse.sprites.front_default}" class="poke-viewer__item--poke-image"/>`;
+          // pokeImage.src = pokeResponse.sprites.front_default;
           let pokeName = capitalizeFirst(pokeResponse.name);
           let pokeNumber = pokeResponse.id;
           let pokeHeight = `${Math.ceil(pokeResponse.height/3.048)}'`;
           let pokeWeight = `${Math.ceil(pokeResponse.weight/4.5359237)} lbs.`;
           let attack1 = pokeResponse.moves[0].move.name;
           let attack2 = pokeResponse.moves[1].move.name;
-          let pokeType1 = pokeResponse.types[0].type.name;
+          let pokeType1 = capitalizeFirst(pokeResponse.types[0].type.name);
           let pokeType2 = "";
           let type1 = document.querySelector(".poke__type-1");
           let type2 = document.querySelector(".poke__type-2");
 
           function getType2() {
             pokeResponse.types.length > 1 ?
-              pokeType2 = pokeResponse.types[1].type.name
+              pokeType2 = capitalizeFirst(pokeResponse.types[1].type.name)
               :
-              pokeType2 = "null"
+              pokeType2 = "Null"
           }
           getType2();
           pokeDescShort.innerHTML = `<p>${pokeName}</p>`;
 
           let genInfoHTML =
               `<div class="poke__general-info--number-name">
-                    <p><img src="http://www.pokeapi-how.appspot.com/favicon.ico" class="poke-ball"/> #${pokeNumber} ${pokeName}</p>
+                    <p><img src="http://www.pokeapi-how.appspot.com/favicon.ico" class="poke-ball"/> # ${pokeNumber} ${pokeName}</p>
                 </div>
                 <div class="poke__general-info--height">
-                    <p>height:</p>
+                    <p>Height:</p>
                     <p>${pokeHeight}</p>
                 </div>
                 <div class="poke__general-info--weight">
-                    <p>weight:</p>
+                    <p>Weight:</p>
                     <p>${pokeWeight}</p>
                 </div>
                 <div class="poke__general-info--poke-description">
@@ -86,24 +87,54 @@ getRandomizedPokemon.addEventListener("click", function() {
             type2.textContent = pokeType2;
 
       }
+      else {
+        console.log("loading");
+        pokeViewer.innerHTML = `<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f53f8d8c-df75-4175-9b3f-686a11dd0a84/dcxk9tz-8a0cd888-96d6-4fed-bc3d-dcb369b7a1bb.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y1M2Y4ZDhjLWRmNzUtNDE3NS05YjNmLTY4NmExMWRkMGE4NFwvZGN4azl0ei04YTBjZDg4OC05NmQ2LTRmZWQtYmMzZC1kY2IzNjliN2ExYmIuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.OVQitgP1JAkcRV-5p22lW2JYlY_xlgM4FoVEMKjH-XM" height="100" width="100"/>`;
+      }
   };
   pokeRequest.open("GET","http://pokeapi.co/api/v2/pokemon/" + getRandomPokemon());
   pokeRequest.send();
 });
 
+// speakButton.addEventListener("click", function(e) {
+//   setTimeout(function(){
+//     var textToSpeak = document.querySelector(".poke__general-info").textContent;
+//     var utterThis = new SpeechSynthesisUtterance(textToSpeak);
+//     utterThis.pitch = 1.2;
+//     utterThis.rate = 1.1;
+//     utterThis.onstart = function(e) {
+//       pokedexAlertsAlert.classList.add("flashing");
+//     }
+//     utterThis.onend = function(e) {
+//       pokedexAlertsAlert.classList.remove("flashing");
+//     }
+//     synth.speak(utterThis);
+//   }, 100);
+//
+// });
+
 speakButton.addEventListener("click", function(e) {
   setTimeout(function(){
     var textToSpeak = document.querySelector(".poke__general-info").textContent;
-    var utterThis = new SpeechSynthesisUtterance(textToSpeak);
-    utterThis.pitch = 1.2;
-    utterThis.rate = 1.1;
-    utterThis.onstart = function(e) {
-      pokedexAlertsAlert.classList.add("flashing");
-    }
-    utterThis.onend = function(e) {
-      pokedexAlertsAlert.classList.remove("flashing");
-    }
-    synth.speak(utterThis);
+    responsiveVoice.speak(textToSpeak, "US English Female", {rate: 1.1});
+    // var utterThis = new SpeechSynthesisUtterance(textToSpeak);
+    // utterThis.pitch = 1.2;
+    // utterThis.rate = 1.1;
+    // utterThis.onstart = function(e) {
+    //   pokedexAlertsAlert.classList.add("flashing");
+    // }
+    // utterThis.onend = function(e) {
+    //   pokedexAlertsAlert.classList.remove("flashing");
+    // }
+    // synth.speak(utterThis);
+    setTimeout(function(){
+      if(responsiveVoice.isPlaying()) {
+        console.log("is playing");
+          pokedexAlertsAlert.classList.add("flashing");
+      } else {
+        console.log("isn't playing");
+          pokedexAlertsAlert.classList.remove("flashing");
+      }
+    }, 100);
   }, 100);
-
 });
